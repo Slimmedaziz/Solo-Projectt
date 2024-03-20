1073e8598ccd9c81ac2092ff6208380e28e8e6d4
slimmedaziz <medazizslim@gmail.com>
2024-03-20 15:59:00 +0100

    XO

--- proj.js
+++ proj.js
@@ -1,25 +1,38 @@
-$(document).ready(function() {
-  $('.card').click(function() {
-    $(this).toggle();
-  });
-});
+var items=document.getElementsByClassName("map")
+let table=["0","1","2"
+          ,"3","4","5"
+          ,"6","7","8"]
+let currentTurn="X"          
+for (const item of items) {
+  item.addEventListener("click",function(){
+     let value=(item.getAttribute("value"))
+     let mapcontent=document.querySelector('.map[value="value"]')
+     mapcontent.innerHTML= currentTurn
 
-$(document).ready(function() {
-    var teams = [];
-    var selectedTeams = [];
-    
-    $('.card').click(function() {
-      var teamName = $(this).data('team');
-      if (selectedTeams.length < 2) {
-        selectedTeams.push(teamName);
-        $(this).slideToggle();
-      }
-      if (selectedTeams.length === 2) {
-        $('#matchup').append('<div>' + selectedTeams[0] + " will face " + selectedTeams[1] + '</div>').slideDown();
-        selectedTeams.forEach(function(team) {
-          $('[data-team="' + team + '"]').slideToggle();
-        });
-        selectedTeams = [];
-      }
-    });
-  });
+     let i=value
+     table[i]=currentTurn
+     if (currentTurn==="X") {
+      currentTurn="O"
+     }
+     else currentTurn="X"
+
+
+  })
+  function evaluate(){
+
+    if (//rows
+      (table[0]===table[1] && table[1]===table[2]) ||
+      (table[3]===table[4] && table[4]===table[5]) ||
+      (table[6]===table[7] && table[7]===table[8]) ||
+      //cols
+      (table[0]===table[3] && table[3]===table[6]) ||
+      (table[1]===table[4] && table[4]===table[7]) ||
+      (table[2]===table[5] && table[5]===table[8]) ||
+      //diag
+      (table[0]===table[4] && table[4]===table[8]) ||
+      (table[2]===table[4] && table[4]===table[6]) 
+      ){
+        
+    }
+  }
+}
